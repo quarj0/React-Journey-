aba7571cf035f494225b4182fc9ecedf3aba7c70
hacks-and-codes <owusuansahkwadwo24@gmail.com>
2022-07-29 08:56:41 +0000

    Starting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactLogo from '../src/images/logo192.png'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div className='container'>
+        <nav className='navbar'>
+            <img src={ReactLogo} alt="React Logo" />
+            <h3 className='reactfacts'>ReactFacts</h3>
+            <h4 className='project1'>React Course Project-1</h4>
+            </nav>
+            <main className='main-contents'>
+            <h1 className='fun'>Fun facts about React</h1>
+              <ul className='list'>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+              </ul>
+            </main>
+        
+    </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
